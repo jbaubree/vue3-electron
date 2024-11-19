371881e47fc1fb90e10b21ec42774c2f967803aa
Jean-Baptiste AUBRÉE <50230578+jbaubree@users.noreply.github.com>
2024-11-19 07:26:10 +0100

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const userStore = useUserStore()
+</script>
+
+<template>
+  <div my-auto flex flex-col items-center gap-5>
+    {{ $t('sentences.connected-user:') }} <pre class="mb-8 text-left">{{ userStore.user }}</pre>
+    <button class="text-sm btn" @click="userStore.logout">
+      {{ $t('actions.logout') }}
+    </button>
+  </div>
+</template>
---
+++ src/pinia.d.ts
@@ -0,0 +1,8 @@
+import type { Router } from 'vue-router'
+import 'pinia'
+
+declare module 'pinia' {
+  export interface PiniaCustomProperties {
+    router: Router
+  }
+}
